8b1e6efefef355d2e8228d6ee4648727252713c9
Tony Quezada <tonyqvargas@gmail.com>
2025-04-19 00:33:37 -0600

    State row functionalities
    Now if a port row is not empty, another port row will appear.

--- src/renderer/src/components/Port.jsx
+++ src/renderer/src/components/Port.jsx
@@ -1,38 +1,44 @@
+// Port.jsx
+
 import '../App.css'
 
-const Port = ( { portName="port_name", disabled=false } ) => {
+const Port = ( { portPlaceholder="port_name", disabled=false, portProperties, onChange} ) => {
 
     if(disabled === true){
         return (
             <div className='flex flex-row mx-9 my-1'>
-                <input disabled type="text" name="port_name" id="port_name" placeholder={"+"}
+                <input disabled type="text" name="portName" id="portName" placeholder={"+"}
                 className='border rounded w-6/12 m-2 px-2 py-0.5 border-gray-300 text-gray-500'
                 />
-                <select disabled name="directionSelect" id="directionSelect"
+                <select disabled name="direction" id="direction"
                 className='border rounded m-2 w-1/12 border-gray-300 text-gray-300'
                 >
                     <option value="in">in</option>
                     <option value="out">out</option>
                 </select>
-                <input disabled type="checkbox" name="busCheckbox" id="busCheckbox" className='m-2 w-1/12 border-gray-300'/>
-                <input disabled type="text" name="MSB" id="MSB" className='border rounded px-2 py-0.5 m-2 w-2/12 border-gray-300'/>
-                <input disabled type="text" name="LSB" id="LSB" className='border rounded px-2 py-0.5 m-2 w-2/12 border-gray-300'/>
+                <input disabled type="checkbox" name="isBus" id="isBus" className='m-2 w-1/12 border-gray-300'/>
+                <input disabled type="text" name="msb" id="msb" className='border rounded px-2 py-0.5 m-2 w-2/12 border-gray-300'/>
+                <input disabled type="text" name="lsb" id="lsb" className='border rounded px-2 py-0.5 m-2 w-2/12 border-gray-300'/>
             </div>
         )
     }
     else{
         return (
             <div className='flex flex-row mx-9 my-1'>
-                <input type="text" name="port_name" id="port_name" placeholder={portName}
+                <input type="text" name="portName" id="portName" placeholder={portPlaceholder}
+                value={portProperties.portName}
+                onChange={e => onChange(portProperties.id, e)}
                 className='border rounded w-6/12 m-2 px-2 py-0.5'
                 />
-                <select name="directionSelect" id="directionSelect" className='border rounded m-2 w-1/12'>
+                <select name="direction" id="direction" className='border rounded m-2 w-1/12'
+                value={portProperties.direction}
+                onChange={e => onChange(portProperties.id, e)}>
                     <option value="in">in</option>
                     <option value="out">out</option>
                 </select>
-                <input type="checkbox" name="busCheckbox" id="busCheckbox" className='m-2 w-1/12'/>
-                <input type="text" name="MSB" id="MSB" className='border rounded px-2 py-0.5 m-2 w-2/12'/>
-                <input type="text" name="LSB" id="LSB" className='border rounded px-2 py-0.5 m-2 w-2/12'/>
+                <input type="checkbox" name="isBus" id="isBus" className='m-2 w-1/12' checked={portProperties.isBus} onChange={e => onChange(portProperties.id, e)}/>
+                <input type="text" name="msb" id="msb" className='border rounded px-2 py-0.5 m-2 w-2/12' value={portProperties.msb} onChange={e => onChange(portProperties.id, e)}/>
+                <input type="text" name="lsb" id="lsb" className='border rounded px-2 py-0.5 m-2 w-2/12' value={portProperties.lsb} onChange={e => onChange(portProperties.id, e)}/>
             </div>
         )
     }
--- src/renderer/src/components/PortTable.jsx
+++ src/renderer/src/components/PortTable.jsx
@@ -1,7 +1,58 @@
+// PortTable.jsx
+
 import '../App.css'
 import Port from "./Port"
+import { useState } from 'react'
 
 const PortTable = () => {
+
+    let counter = 0;
+    const [portState, setPortState] = useState([
+        {
+        "id": ++counter,
+        "portName": "",
+        "direction": "in",
+        "isBus": false,
+        "msb": "",
+        "lsb": ""
+        }
+    ])
+
+    const handlePortChange = (id, e) => {
+        const name = e.target.name
+        let newValue
+
+        if(e.target.type === 'checkbox')
+            newValue = e.target.checked
+        else
+            newValue = e.target.value
+        
+        setPortState(previousPortState => {
+            const updatedPortsArray = previousPortState.map(portObject => {
+ 
+                if(portObject.id === id)
+                    return {...portObject, [name]: newValue}
+                else
+                    return portObject
+            })
+
+            if(newValue != "" && newValue != false && id === previousPortState[previousPortState.length - 1]["id"]){
+                const newPortObject = {
+                    "id": id + 1,
+                    "portName": "",
+                    "direction": "in",
+                    "isBus": false,
+                    "msb": "",
+                    "lsb": ""
+                }
+                return [...updatedPortsArray, newPortObject]
+            }
+            else{
+                return updatedPortsArray
+            }
+
+        })
+    }
 
     return (
         <>
@@ -14,9 +65,15 @@ const PortTable = () => {
             </div>
 
             <form>
-                <Port/>
-                <Port/>
-                <Port/>
+                <div>
+                    {
+                        portState.map((portObject) => {
+                            return (
+                                <Port key={portObject.id} portProperties={portObject} onChange={handlePortChange} />
+                            )
+                        })
+                    }
+                </div>
                 <Port disabled={true} />
             </form>
         </>
